c5186ba2977371275689672e380f64f316f4bfe0
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-16 07:23:36 -0800

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NotFound from './Components/NotFound/NotFound';
+import SingleWordDetails from './Components/SingleWordDetails/SingleWordDetails';
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Router>
+          <Switch>
+
+            <Route path="/home">
+              <Home />
+            </Route>
+
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/word/:id">
+              <SingleWordDetails />
+            </Route>
+
+            <Route path="*">
+              <NotFound />
+            </Route>
+
+          </Switch>
+        </Router>
+        
+    </>
   );
 }
 
